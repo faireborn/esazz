583ddd7ec28930c52b9a83f0e3b66513847e3708
Ryuto Saito <r.saito0111@gmail.com>
2025-10-31 14:56:21 +0900

    delete: rm Unused test

--- src/main.zig
+++ src/main.zig
@@ -12,14 +12,3 @@ test "simple test" {
     try list.append(gpa, 42);
     try std.testing.expectEqual(@as(i32, 42), list.pop());
 }
-
-test "fuzz example" {
-    const Context = struct {
-        fn testOne(context: @This(), input: []const u8) anyerror!void {
-            _ = context;
-            // Try passing `--fuzz` to `zig build test` and see if it manages to fail this test case!
-            try std.testing.expect(!std.mem.eql(u8, "canyoufindme", input));
-        }
-    };
-    try std.testing.fuzz(Context{}, Context.testOne, .{});
-}
